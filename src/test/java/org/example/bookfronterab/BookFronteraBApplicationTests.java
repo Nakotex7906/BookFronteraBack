fa5f2d65c32f6b67dd1121887aa9df68e6918844
Ignacio Essus <i.essus01@ufromail.cl>
2025-08-22 14:51:20 -0400

    Prime commit

---
+++ src/test/java/org/example/bookfronterab/BookFronteraBApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookfronterab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookFronteraBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
