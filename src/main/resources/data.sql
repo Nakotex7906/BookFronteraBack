d77411d2e75e0ce0b9f77162ee809459804a2406
Ignacio Essus <i.essus01@ufromail.cl>
2025-11-08 18:51:07 -0300

    Usamos docker compose para tener una base de datos dockerizada

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-MERGE INTO rooms (name, capacity, active) KEY(name) VALUES ('Sala A', 4, true);
-MERGE INTO rooms (name, capacity, active) KEY(name) VALUES ('Sala B', 6, true);
-MERGE INTO rooms (name, capacity, active) KEY(name) VALUES ('Sala C', 8, true);
