ddfdb50c9256a29c3482ab61c82cd33587369995
ElLuck15 <71898499+ElLuck15@users.noreply.github.com>
2025-11-10 21:00:57 -0300

    feat
    BookFronteraApplicationTest y TestContainers activados

--- src/test/java/bookfronterab/BookFronteraBApplicationTests.java
+++ src/test/java/bookfronterab/BookFronteraBApplicationTests.java
@@ -2,12 +2,34 @@ package bookfronterab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class BookFronteraBApplicationTests {
 
+    @Container
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("bookfronterab-test")
+            .withUsername("testuser")
+            .withPassword("testpass");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.PostgreSQLDialect");
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create"); // 'create' para que cree las tablas en la BBDD limpia
+    }
+
     @Test
     void contextLoads() {
+        System.out.println("Contexto cargado y conectado a la BBDD de Testcontainers!");
     }
 
 }
