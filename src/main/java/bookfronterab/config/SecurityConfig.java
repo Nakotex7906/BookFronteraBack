42beded6d04d2ff852d1a428bd7277ca3ca3a5d7
FernandoJelvez <f.jelvez02@ufromail.cl>
2025-11-22 19:08:58 -0300

    Se reemplazó el uso de un metodo lambda por una referencia a método HeadersConfigurer.FrameOptionsConfig::sameOrigin en SecurityConfig

--- src/main/java/bookfronterab/config/SecurityConfig.java
+++ src/main/java/bookfronterab/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -27,7 +28,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(corsConfig()))
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**").disable())
-                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/",                // raíz
