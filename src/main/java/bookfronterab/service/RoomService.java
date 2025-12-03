21355161f1b2a0246560b6af230f6199e7a9f290
ElLuck15 <71898499+ElLuck15@users.noreply.github.com>
2025-12-03 05:45:07 -0300

    refactor(config)
    refactor para SecurityConfig

--- src/main/java/bookfronterab/service/RoomService.java
+++ src/main/java/bookfronterab/service/RoomService.java
@@ -1,5 +1,6 @@
 package bookfronterab.service;
 
+import bookfronterab.exception.ImageUploadException;
 import bookfronterab.exception.ResourceNotFoundException;
 import bookfronterab.dto.RoomDto;
 import bookfronterab.model.Room;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional; // <-- 1. IMPORTA ESTO
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -37,8 +39,8 @@ public class RoomService {
         if (imageFile != null && !imageFile.isEmpty()) {
             try {
                 imageUrl = cloudinaryService.uploadFile(imageFile);
-            } catch (Exception e) {
-                throw new RuntimeException("Error al subir imagen a Cloudinary", e);
+            } catch (IOException e) {
+                throw new ImageUploadException("Error al subir imagen a Cloudinary", e);
             }
         }
 
@@ -79,8 +81,8 @@ public class RoomService {
             try {
                 String newUrl = cloudinaryService.uploadFile(imageFile);
                 existingRoom.setImageUrl(newUrl);
-            } catch (Exception e) {
-                throw new RuntimeException("Error al actualizar imagen en Cloudinary", e);
+            } catch (IOException e) {
+                throw new ImageUploadException("Error al actualizar imagen en Cloudinary", e);
             }
         }
 
