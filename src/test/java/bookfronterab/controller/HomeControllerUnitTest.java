e5ec382890d99f5107d07b3c3e7cdf6b85838db5
Ignacio Essus <i.essus01@ufromail.cl>
2025-12-08 22:27:52 -0300

    fix, agregue un mockitobean para que pase

--- src/test/java/bookfronterab/controller/HomeControllerUnitTest.java
+++ src/test/java/bookfronterab/controller/HomeControllerUnitTest.java
@@ -1,9 +1,11 @@
 package bookfronterab.controller;
 
+import bookfronterab.service.RateLimitingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -11,18 +13,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(HomeController.class)
-@AutoConfigureMockMvc(addFilters = false) // Desactiva login para que sea público
+@AutoConfigureMockMvc(addFilters = false) // Desactiva seguridad/filtros
 class HomeControllerUnitTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    // Mockear la dependencia que rompe el contexto
+    @MockitoBean
+    private RateLimitingService rateLimitingService;
 
     @Test
     void home_DeberiaRetornarInformacionBasica() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk()) // Esperamos un 200 OK
                 .andExpect(jsonPath("$.app").value("BookFrontera")) // Verificamos que diga el nombre
-                .andExpect(jsonPath("$.apiIndex").exists()) // Verificamos que existan las otras claves
+                .andExpect(jsonPath("$.apiIndex").exists())
                 .andExpect(jsonPath("$.docs").exists());
     }
 }
